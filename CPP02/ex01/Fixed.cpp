128cdf0399637d5b409751640c64423807efbe90
Riku Ishibashi <rishibas@c3r4s2.42tokyo.jp>
2026-01-04 03:29:54 +0900

    浮動小数点数と固定小数点数の勉強

---
+++ CPP02/ex01/Fixed.cpp
@@ -0,0 +1,41 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : FixedPointNum(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int num) {
+	std::cout << "Int constructor called" << std::endl;
+	FixedPointNum = num << FractionalBits;
+} 
+
+Fixed::Fixed(const float num){
+	std::cout << "Float constructor called" << std::endl;
+	FixedPointNum = (int)((num * 256) + 0.5);
+}
+
+Fixed::Fixed(const Fixed &other){
+	std::cout << "Copy constructor called" << std::endl;
+	this->FixedPointNum = other.getRawBits();
+}
+
+Fixed& Fixed::operator=(const Fixed &other){
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other){
+		this->FixedPointNum = other.getRawBits();
+	}
+	return (*this);
+}
+
+Fixed::~Fixed(){
+std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits(void) const{
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->FixedPointNum;
+}
+
+void	Fixed::setRawBits(int const raw){
+	FixedPointNum = raw;
+}
---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,22 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed{
+	private:
+		int 				FixedPointNum;
+		static const int	FractionalBits = 8;
+	
+	public:
+		Fixed();
+		Fixed(const int num);
+		Fixed(const float num);
+		Fixed(const Fixed &other);
+		Fixed&	operator=(const Fixed &other);
+		~Fixed();
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
+
+#endif
