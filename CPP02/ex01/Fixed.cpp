ff5a0b6ec0e4c34ce561ec342666a999a52035e3
Riku Ishibashi <rishibas@c4r2s4.42tokyo.jp>
2026-01-05 05:08:44 +0900

    coplete: CPP03

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -44,8 +44,3 @@ std::ostream &operator<<(std::ostream &out, const Fixed &fixed){
 	out << fixed.toFloat();
 	return out;
 }
-
-
-std::ostream &operator>(const Fixed &fixed, const Fixed &fixed1){
-	if (fixed.)
-}
